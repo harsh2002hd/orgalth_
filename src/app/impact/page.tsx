21ac9fab4bba50ae22fd5709f71a6f0adabb7d3c
Harsh Sharma <hd2002sharma@gmail.com>
2026-04-29 20:39:25 +0530

    error fixed

--- src/app/impact/page.tsx
+++ src/app/impact/page.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
 import { useLanguage } from '@/context/LanguageContext';
-import { useLanguage } from '@/context/LanguageContext';
 
 export default function Impact() {
   const { t } = useLanguage();
